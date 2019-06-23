417889cce5ed84b024ee30bccb9ec84e10b0e268
dimansf <diman18sf@gmail.com>
2019-06-23 21:09:05 +0300

    в пизду

---
+++ BagTask/BagTasker.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BagTask
+{
+	public class BagTasker
+	{
+		/// <summary>
+		/// 
+		/// </summary>
+		int[][] orders;
+		/// <summary>
+		/// 
+		/// </summary>
+		int total;
+		/// <summary>
+		/// 
+		/// </summary>
+		int ostatok;
+		/// <summary>
+		/// 
+		/// </summary>
+		int liquid;
+		/// <summary>
+		/// 
+		/// </summary>
+		List<(string, int)> conds;
+		/// <summary>
+		/// 
+		/// </summary>
+		int wSaw;
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="orders"></param>
+		/// <param name="store"></param>
+		/// <param name="liqCondition"></param>
+		/// <returns></returns>
+		public Dictionary<(int, int), List<(string, int)>> calculate(int[][] orders, int[][] store, bool liqCondition = true)
+		{
+			var dat = new Dictionary<(int, int), List<(string, int)>>();
+
+			store.Select(el =>
+			{
+				//[ид, длина, кол - во, ликвид, макс.обр, номер склада]
+				var temp = calc(orders, el[1]);
+				if (liqCondition)
+				{
+					temp = LiqSelect(temp, el[3], el[4]);
+				}
+				dat[(el[1], el[5])] = temp;
+
+				return 0;
+			});
+			
+			return dat;
+		}
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="combs"></param>
+		/// <param name="liq"></param>
+		/// <param name="obr"></param>
+		/// <returns></returns>
+		private List<(string, int)> LiqSelect(List<(string, int)> combs , int liq, int obr)
+		{
+			var res = new List<(string, int)>();
+			foreach (var el in combs)
+			{
+				if (el.Item2 >= liq || el.Item2 <= obr)
+					res.Add(el);
+			}
+			return res;
+
+		}
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="orders"></param>
+		/// <param name="total"></param>
+		/// <param name="widthWa"></param>
+		/// <returns></returns>
+		private List<(string, int)> calc(int[][] orders, int total, int widthWa = 4)
+		{
+			this.orders = orders.Select(el => new int[] { el[0], el[1] + widthWa }).ToArray();
+			this.total = total;
+
+
+			// [строка сложенных элементов, остаток от доски]
+			conds = new List<(string, int)>();
+			Deeper(0, 0, conds, "", 0);
+
+			return conds;
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="depth"></param>
+		/// <param name="currSum"></param>
+		/// <param name="conds"></param>
+		/// <param name="condStr"></param>
+		/// <param name="counter"></param>
+		private void Deeper(int depth, int currSum, List<(string, int)> conds, string condStr, int counter)
+		{ 
+			if (depth >= orders.Length)
+			{
+				return;
+			}
+			for (var i = 0; i <= orders[depth][0]; i++)
+			{
+				var s = orders[depth][1] * i + currSum;
+				if (s > this.total) return;
+				var forAppend = condStr;
+
+				if (i != 0 )
+				{
+					if (depth > 0 && condStr.Length > 0)
+						forAppend = condStr + " + " + this.orders[depth][1]+ $"({this.orders[depth][3]})" + "*" + i ;
+					else
+					{
+						forAppend = condStr + this.orders[depth][1] + $"({this.orders[depth][3]})"+ '*' + i;
+					}
+				}
+				// forAppend = condsStr + this.orders[depth][1] + '*' + i ;
+				if (counter + i != 0)
+					if (!HasKey(forAppend))
+						conds.Add( (forAppend, total - s));
+
+				Deeper(depth + 1, s, conds, forAppend, counter + i);
+			}
+			
+		}
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="keyStr"></param>
+		/// <returns></returns>
+		public bool HasKey(string keyStr)
+		{
+			var flag = false;
+			conds.Select(element => {
+				if (element.Item1 == keyStr)
+				{
+					flag = true;
+					return 0;
+				}
+				return 0;
+			});
+			return flag;
+		}
+	}
+}
+	
---
+++ Raspil/RaspilOperator.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Raspil
+{
+
+	public class RaspilOperator
+	{
+
+		public RaspilOperator()
+		{
+		}
+		/// <summary>
+		/// 1. Самый лучшая обрезь та, что total - ostatok больше всего, значит доска использовалась по полной 
+		/// пример: 1500 - 500 = 1000 или 6000 - 2000 = 4000
+		/// получается, что 6000 - 4000 = 2000 полезной нагрузки
+		/// 1500 - 1000 = 500 полезной нагрузки
+		/// очевидно выбрать стоит выбрать 6000
+		/// </summary>
+		public void algoritm1(int[][] orders, int[][] store)
+		{
+
+			var mapRaspil = new List<List<(int, (string, int, int))>>();
+			var bt = new BagTask.BagTasker();
+			var unicalID = orders.Select(row => row[0]).Distinct().ToArray();
+
+			while (checkOrders(orders))
+			{
+				// {ид доски => {длина доски, номер склада => ["комбинация", остаток] } }
+				var res = new Dictionary<int, Dictionary<(int, int), List<(string, int)>>>();
+
+				// 1 этап генерация комбинаций для каждого ордера
+				unicalID.Select(el =>
+				{
+
+					res[el] = bt.calculate(selectByID(orders, el), selectByID(store, el));
+
+					if (res[el].Count == 0)
+						res[el] = bt.calculate(selectByID(orders, el), selectByID(store, el), false);
+
+					return 0;
+				});
+
+				// 2 выбор лучших элементов
+				var bests = new List<(int, (string, int, int))>();
+				foreach (var el in res)
+				{
+					bests.Add((el.Key, getBestCombination(el.Value)));
+				}
+
+				// вычесть лучшие просчеты и добавить в карту распила
+
+				mapRaspil.Add(bests);
+
+				subtractBestVectorValues();
+			}
+		}
+
+		private void subtractBestVectorValues(int[][] orders, int[][] store, List<(int, (string, int, int))> best)
+		{											// ид доски => ["комбинация", длина доски, номер склада]
+			
+			foreach (var el in best) {
+				var els = el.Item2.Item1.Split('+');
+
+			}
+		}
+		private int[][] selectByID(int[][] orders, int id) {
+			var ls = new List<int[]>();
+			foreach (var el in orders)
+			{
+				if (el[0] == id)
+					ls.Add(el);
+			}
+			return ls.ToArray();
+		}
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="combs">
+		/// 1 параметр длина 6000, номер склада 1
+		/// 2 лист комбинаций заказов ("строка комбинаций", остаток)
+		/// </param>
+		private (string, int, int) getBestCombination(Dictionary<(int, int), List<(string, int)>> combs)
+		{
+			// строка комбинации, длина палки, склад
+			var bcomb = ("", 0, 0);
+			foreach (var row in combs)
+			{
+				foreach (var el in row.Value)
+				{
+					// если остаток 1500 - 500 = 1000
+					// если 6000 - 1000 = 5000
+					// полезный материал
+					if (row.Key.Item1 - bcomb.Item2 < row.Key.Item1 - el.Item2)
+						bcomb = (el.Item1, row.Key.Item1, row.Key.Item2);
+				}
+			}
+			return bcomb;
+		}
+
+		private bool checkOrders(int[][] orders)
+		{
+			if (orders.Length > 0) return true;
+			return false;
+		}
+		/// <summary>
+		/// В первую очередь расходуем всю обрезь со склада, по условиям ликвида, но без условий полезной нагрузки
+		/// Остальное по первому алгоритму
+		/// </summary>
+		public void algoritm2()
+		{
+
+		}
+		/// <summary>
+		/// В первую очередь расходуем обрезь, без условий ликвида и полезной нагрузки
+		/// Остальное по первому алгоритму
+		/// </summary>
+		public void algoritm3()
+		{
+
+		}
+
+
+
+	}
+}
